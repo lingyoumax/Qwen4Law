5ff510d94620739b3c89954601d4ae0709f490c8
Ye Tao <lingyoumax@gmail.com>
2025-07-17 16:04:30 +0800

    Update test.py

--- test.py
+++ test.py
@@ -2,14 +2,11 @@ from modelscope import AutoTokenizer
 
 tokenizer = AutoTokenizer.from_pretrained('Tokenizer', padding_side='left')
 
-# 原始字符串
 text = "中国"
-print(tokenizer.tokenize("中国"))
 
-
-# 编码为 token IDs（默认返回 dict）
 encoded = tokenizer(text)
 
 print("input_ids:", encoded['input_ids'])
 print("token_type_ids:", encoded.get('token_type_ids'))
 print("attention_mask:", encoded['attention_mask'])
+print("tokens",tokenizer.convert_ids_to_tokens(encoded['input_ids']))
