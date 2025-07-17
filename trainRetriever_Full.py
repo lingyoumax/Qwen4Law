87963e64e8f0cd9b9042d3c7552c3ea22930e17a
Ye Tao <lingyoumax@gmail.com>
2025-07-17 17:14:44 +0800

    Update trainRetriever_Full.py

--- trainRetriever_Full.py
+++ trainRetriever_Full.py
@@ -1,5 +1,6 @@
 from modelscope import AutoConfig, AutoModel, AutoTokenizer
+from settings import device
 
 config = AutoConfig.from_pretrained("Qwen/Qwen3-Embedding-0.6B")
-model = AutoModel.from_config(config)
+model = AutoModel.from_config(config).to(device)
 tokenizer = AutoTokenizer.from_pretrained('RetrieverTokenizer', padding_side='left')
