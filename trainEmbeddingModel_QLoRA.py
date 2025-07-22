7a059622410bd567a7b52fdc7d0d9d13395a2b3f
Ye Tao <lingyoumax@gmail.com>
2025-07-22 17:33:29 +0800

    Update trainEmbeddingModel_QLoRA.py

--- trainEmbeddingModel_QLoRA.py
+++ trainEmbeddingModel_QLoRA.py
@@ -1,4 +1,32 @@
-from modelscope import AutoModel, AutoTokenizer
+from modelscope import AutoTokenizer, BitsAndBytesConfig
+from peft import TaskType, prepare_model_for_kbit_training, LoraConfig, get_peft_model
+from modelscope import AutoModel
+import torch
 
-tokenizer = AutoTokenizer.from_pretrained('Qwen/Qwen3-Embedding-0.6B', padding_side='left')
-model = AutoModel.from_pretrained("Qwen/Qwen3-Embedding-0.6B")
+tokenizer = AutoTokenizer.from_pretrained("Qwen/Qwen3-Embedding-0.6B", padding_side="left")
+
+bnb_config = BitsAndBytesConfig(
+    load_in_4bit=True,
+    bnb_4bit_use_double_quant=True,
+    bnb_4bit_quant_type="nf4",
+    bnb_4bit_compute_dtype=torch.float16,
+)
+
+model = AutoModel.from_pretrained(
+    "Qwen/Qwen3-Embedding-0.6B",
+    quantization_config=bnb_config,
+    device_map="auto"
+)
+
+model = prepare_model_for_kbit_training(model)
+
+lora_config = LoraConfig(
+    r=16,
+    lora_alpha=32,
+    target_modules=["q_proj", "k_proj", "v_proj", "o_proj"],  # 修改为适配你模型的模块名（建议print(model)后确认）
+    lora_dropout=0.1,
+    bias="none",
+    task_type=TaskType.FEATURE_EXTRACTION  # 对应嵌入模型任务
+)
+
+model = get_peft_model(model, lora_config)
