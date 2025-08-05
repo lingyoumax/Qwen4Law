0a6d9adfb6a3ae3210fbff4bb873b39957f34921
Ye Tao <lingyoumax@gmail.com>
2025-08-05 09:57:26 +0800

    Update settings.py

--- settings.py
+++ settings.py
@@ -5,5 +5,5 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 random_seed=7
 num_negative_docs=10
 embedding_max_length = 512
-embedding_batch_size = 8
+embedding_batch_size = 4
 embedding_test_ratio=0.2
