71f32d48b2366c36c4c27e923c1e22d5b67dd7fc
Ye Tao <lingyoumax@gmail.com>
2025-08-08 14:17:10 +0800

    Update trainEmbeddingModel_QLoRA.py

--- trainEmbeddingModel_QLoRA.py
+++ trainEmbeddingModel_QLoRA.py
@@ -170,7 +170,7 @@ for epoch in tqdm(range(n_epoch), desc="Training"):
     if recall > best_recall:
         best_recall = recall
         model.save_pretrained(savePath)
-        torch.save(model.state_dict(), f'{savePath}/EmbeddingModel_QLoRA_Best.pth')
+        torch.save(model.state_dict(), f'{savePath}/{savePath}_Best.pth')
 
-torch.save(model.state_dict(), f'{savePath}/EmbeddingModel_QLoRA_Final.pth')
+torch.save(model.state_dict(), f'{savePath}/{savePath}_Final.pth')
 drawEmbeddingLoss(savePath, Loss, Recall)   
