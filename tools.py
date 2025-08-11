d4a3e18b3315fbded1182d7264c610aa6961826c
Ye Tao <lingyoumax@gmail.com>
2025-08-11 10:59:46 +0800

    Update tools.py

--- tools.py
+++ tools.py
@@ -9,6 +9,7 @@ import numpy as np
 from settings import device, num_negative_docs
 
 def getFiles(directory='laws', fileend='.txt'):
+    # 获取文件夹下指定后缀的所有文件的文件名
     txt_files = []
     for filename in os.listdir(directory):
         if filename.endswith(fileend):
@@ -24,12 +25,13 @@ def last_token_pool(last_hidden_states, attention_mask):
         sequence_lengths = attention_mask.sum(dim=1) - 1
         batch_size = last_hidden_states.shape[0]
         return last_hidden_states[torch.arange(batch_size, device=device), sequence_lengths]
-    
+ 
 def evaluateEmbeddingModel(model, dataloader, temperature):
+    # 计算模型在测试集上的Loss
     model.eval()
     l = 0
     total = len(dataloader)
-    with torch.no_grad():
+    with torch.inference_mode():
         for batch in dataloader:
             query_dict=BatchEncoding({"input_ids":batch["query_input_ids"],"attention_mask":batch["query_attention_mask"]})
             query_output = model(**query_dict)
@@ -72,6 +74,46 @@ def evaluateEmbeddingModel(model, dataloader, temperature):
             
     model.train()
     return l/total if total > 0 else 0
+
+def evaluateTrainedEmbeddingModel(model, dataloader, temperature):
+    # 计算模型在测试集上的分离度
+    model.eval()
+    margin = 0
+    total = 0
+    with torch.inference_mode():
+        for batch in dataloader:
+            query_dict=BatchEncoding({"input_ids":batch["query_input_ids"],"attention_mask":batch["query_attention_mask"]})
+            query_output = model(**query_dict)
+            query_embedding = last_token_pool(query_output.last_hidden_state, batch["query_attention_mask"])#batch_size * embedding_length
+            query_embedding = F.normalize(query_embedding, dim=1)
+
+            positive_dict=BatchEncoding({"input_ids":batch["positive_input_ids"],"attention_mask":batch["positive_attention_mask"]})
+            positive_output = model(**positive_dict)
+            positive_embedding = last_token_pool(positive_output.last_hidden_state, batch["positive_attention_mask"])#batch_size * embedding_length
+            positive_embedding = F.normalize(positive_embedding, dim=1)
+
+            negative_embeddings=[]
+            for i in range(num_negative_docs):
+                negative_dict_i=BatchEncoding({"input_ids":batch[f"negative_input_ids_{i}"],"attention_mask":batch[f"negative_attention_mask_{i}"]})
+                negative_output_i = model(**negative_dict_i)
+                negative_embedding_i = last_token_pool(negative_output_i.last_hidden_state, batch[f"negative_attention_mask_{i}"])#batch_size * embedding_length
+                negative_embedding_i = F.normalize(negative_embedding_i, dim=1)
+                negative_embeddings.append(negative_embedding_i)
+            
+            B = query_embedding.size(0)
+
+            negatives_stacked = torch.stack(negative_embeddings)
+
+            sim_q_pos = torch.sum(query_embedding * positive_embedding, dim=1) / temperature
+            sim_q_negs=torch.sum(query_embedding.unsqueeze(0) * negatives_stacked, dim=2).T / temperature
+            sim_q_neg=torch.amax(sim_q_negs, dim=1)
+            
+            margin = margin + torch.sum(sim_q_pos-sim_q_neg).item()
+            total = total + B
+
+            
+    model.train()
+    return margin/total if total > 0 else 0
 
 def drawEmbeddingLoss(saveName, TrainLoss, TestLoss):
     plt.figure(figsize=(20, 10))
