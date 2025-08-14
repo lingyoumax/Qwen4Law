72da19a24906080cafdf91fbbfcb5c3e2057c725
Ye Tao <lingyoumax@gmail.com>
2025-08-14 15:46:33 +0800

    Update settings.py

--- settings.py
+++ settings.py
@@ -2,6 +2,7 @@ import torch
 
 embedding_modelname = "Qwen/Qwen3-Embedding-0.6B"
 reranker_modelname = "Qwen/Qwen3-Reranker-0.6B"
+llm_modelname = "Qwen/Qwen3-8B"
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 random_seed = 7
 num_negative_docs = 10
