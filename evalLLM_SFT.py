980f79cb69c12f3448b6f7b2409f3e8cf1043486
Ye Tao <lingyoumax@gmail.com>
2025-08-25 09:22:39 +0800

    update codes to eval LLM before and after SFT

--- evalLLM_SFT.py
+++ evalLLM_SFT.py
@@ -1,5 +1,6 @@
 import torch
 from modelscope import AutoTokenizer, AutoModelForCausalLM
+from peft import PeftModel
 from datasets import Dataset
 import pandas as pd
 from tqdm.auto import tqdm
@@ -24,7 +25,7 @@ dataset = Dataset.from_list([row_to_sample(row) for _, row in df.iterrows()])
 dataset_split = dataset.train_test_split(test_size=llm_test_ratio, seed=random_seed)
 test_dataset  = dataset_split["test"]
 
-model = AutoModelForCausalLM.from_pretrained(
+base_model = AutoModelForCausalLM.from_pretrained(
     llm_modelname,
     device_map="auto",
     torch_dtype=torch.bfloat16,
@@ -34,6 +35,10 @@ tokenizer = AutoTokenizer.from_pretrained(llm_modelname, trust_remote_code=True)
 if tokenizer.pad_token is None:
     tokenizer.pad_token = tokenizer.eos_token
 
+model = PeftModel.from_pretrained(
+    base_model, 
+    "LLM_SFT"
+)
 model.eval()
 Candidate = []
 Answers = []
@@ -65,7 +70,6 @@ for sample in tqdm(test_dataset):
     answer = tokenizer.decode(output_ids[index:], skip_special_tokens=True).strip("\n")
     Candidate.append(candidate)
     Answers.append(answer)
-
 p, r, f = score(Candidate, Answers, lang="zh", verbose=True, model_type="bert-base-chinese", device=device)
 p = np.array(p)
 r = np.array(r)
@@ -96,4 +100,4 @@ plt.xlabel('F1')
 plt.ylabel('Count')
 plt.legend(loc="upper left")
 
-plt.savefig("evalLLM_based.svg")
+plt.savefig("evalLLM_SFT.svg")
